80825f726e72f3fae63673959ff1fa4ec818c711
Bryan Biedenkapp <gatekeep@gmail.com>
2025-03-19 23:15:25 -0400

    perform startup error check to ensure that libvocoder.dll was downloaded, if it doesn't exist dump out;

--- DVMConsole/App.xaml.cs
+++ DVMConsole/App.xaml.cs
@@ -8,10 +8,16 @@
 * @license AGPLv3 License (https://opensource.org/licenses/AGPL-3.0)
 *
 *   Copyright (C) 2025 Caleb, K4PHP
+*   Copyright (C) 2025 Bryan Biedenkapp, N2PLL
 *
 */
 
+using System.IO;
+using System.Reflection;
 using System.Windows;
+using MessageBox = System.Windows.Forms.MessageBox;
+using MessageBoxButtons = System.Windows.Forms.MessageBoxButtons;
+using MessageBoxIcon = System.Windows.Forms.MessageBoxIcon;
 
 namespace dvmconsole
 {
@@ -20,6 +26,26 @@ namespace dvmconsole
     /// </summary>
     public partial class App : Application
     {
-        /* stub */
-    }
+        /*
+        ** Methods
+        */
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        public App()
+        {
+            string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+
+            if (!File.Exists(Path.Combine(new string[] { Path.GetDirectoryName(path), "libvocoder.DLL" })))
+            {
+                MessageBox.Show("libvocoder is missing or not found! The library is required for operation of the console, please see: https://github.com/DVMProject/dvmvocoder.", "Digital Voice Modem - Desktop Dispatch Console",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Current.Shutdown();
+                return;
+            }
+        }
+    } // public partial class App : Application
 } // namespace dvmconsole
