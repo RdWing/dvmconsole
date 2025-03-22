a37fccfdac2887e61b1630f3fea97b479f627d6a
Bryan Biedenkapp <gatekeep@gmail.com>
2025-03-22 18:34:16 -0400

    ensure the identity string is never empty, if it is replace it with the peer ID;

--- dvmconsole/PeerSystem.cs
+++ dvmconsole/PeerSystem.cs
@@ -74,6 +74,9 @@ namespace dvmconsole
 
             string software = $"CONSOLE_R{_SEM_VERSION.Major.ToString("D2")}A{_SEM_VERSION.Minor.ToString("D2")}";
 
+            if (system.Identity.Length == 0)
+                system.Identity = system.PeerId.ToString();
+
             // set configuration parameters
             peer.Passphrase = system.Password;
             peer.Information = new PeerInformation
