886c0f44de5ee24629785f6f1f9923ec6a3ddc2a
Steven Jennison <stvnjennison@gmail.com>
2025-03-22 09:46:27 -0400

    Add file header to KeyContainer.cs

--- dvmconsole/KeyContainer.cs
+++ dvmconsole/KeyContainer.cs
@@ -1,4 +1,17 @@
-﻿namespace dvmconsole;
+﻿// SPDX-License-Identifier: AGPL-3.0-only
+/**
+* Digital Voice Modem - Desktop Dispatch Console
+* AGPLv3 Open Source. Use is subject to license terms.
+* DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+*
+* @package DVM / Desktop Dispatch Console
+* @license AGPLv3 License (https://opensource.org/licenses/AGPL-3.0)
+*
+*   Copyright (C) 2025 Steven Jennison, KD8RHO
+*
+*/
+
+namespace dvmconsole;
 
 /// <summary>
 /// POCO which is used to decode a YML keyfile
